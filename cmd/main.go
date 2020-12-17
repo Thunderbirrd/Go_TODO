c5f931a67d16a681b33106ac32e38e26ee5ac777
Artem <artemis.gorb.14@gmail.com>
2020-12-17 11:16:51 +0300

    base part finished | server is working

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/Thunderbirrd/Go_TODO"
+	"github.com/Thunderbirrd/Go_TODO/pkg/handler"
+	"log"
+)
+
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil{
+		log.Fatalf("Error occured while running http server: %s", err.Error())
+	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Thunderbirrd/Go_TODO
+
+go 15.6
+
+require github.com/gin-gonic/gin v1.6.3
