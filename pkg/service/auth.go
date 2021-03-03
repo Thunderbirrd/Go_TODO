df31af512c16e58e244dec231521b80028e19dff
Artem <artemis.gorb.14@gmail.com>
2021-03-03 20:31:19 +0300

    jwt tokens generating done

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,25 +5,51 @@ import (
 	"fmt"
 	todo "github.com/Thunderbirrd/Go_TODO"
 	"github.com/Thunderbirrd/Go_TODO/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
-const salt = "damd3665flksmdfs1734kmf"
+const (
+	salt       = "damd3665flksmdfs1734kmf"
+	tokenTTL   = 12 * time.Hour
+	signingKey = "fdsfds#4ss6%fmdfDS"
+)
 
 type AuthService struct {
 	repo repository.Authorization
-	
+}
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
 }
 
 func NewAuthService(repo repository.Authorization) *AuthService {
 	return &AuthService{repo: repo}
 }
 
-func (s *AuthService) CreateUser(user todo.User) (int, error){
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
 }
 
-func generatePasswordHash(password string) string{
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
+}
+
+func generatePasswordHash(password string) string {
 	hash := sha1.New()
 	hash.Write([]byte(password))
 
